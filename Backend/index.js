9aae3972531feb049d51d31fbecf558112d8890b
Piyush2205 <pk.rv.668@gmail.com>
2024-03-23 12:46:30 +0530

    FoodDost App V1 -updated

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ require("dotenv").config()
 
 const connection = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGO_URI}`, {
+        await mongoose.connect(`${process.env.MONGODB_URI}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
