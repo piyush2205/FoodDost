b0cc3aac2987a5876a65051e1da24fde0a4e9609
Piyush2205 <pk.rv.668@gmail.com>
2024-03-23 16:04:31 +0530

    FoodDost App V1 -updated

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: "https://food-dost.vercel.app",
+    origin: ["https://food-dost.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     // allowedHeaders: ["Content-Type"],
     credentials: true
