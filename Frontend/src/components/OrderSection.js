a2420d5bb5d9ad21f2244d8c65aaa824212757e2
Piyush2205 <pk.rv.668@gmail.com>
2024-04-03 21:01:48 +0530

    FoodDost App V1 -updated

--- Frontend/src/components/OrderSection.js
+++ Frontend/src/components/OrderSection.js
@@ -140,10 +140,12 @@ function OrderSection() {
                 </div> :
                     <>
                         <div className="flex min-h-screen items-center justify-center">
+                            <h1>wait for some time api is calling </h1>
                             <div className="w-1/3">
                                 <div className="max-w-sm rounded overflow-hidden shadow-lg animate-pulse">
                                     <div className="h-48 bg-gray-300"></div>
                                     <div className="px-6 py-4">
+
                                         <div className="h-6 bg-gray-300 mb-2"></div>
                                         <div className="h-4 bg-gray-300 w-2/3"></div>
                                     </div>
